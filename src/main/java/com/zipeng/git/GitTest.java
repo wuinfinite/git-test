fab495e94856da4388e2b8f11c450f5edd4abec1
Zipeng <1452649735@qq.com>
2022-10-11 13:08:30 +0800

    first commit

---
+++ src/main/java/com/zipeng/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zipeng.git;
+
+/**
+ * @author zipeng
+ * @create &{YEAR}-10-11-13:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
